5e33a4b5865b4e202ba444c2716a3f2f0abfb711
lito00356 <tomasz.liksza@gmail.com>
2026-05-18 22:54:57 +0200

    adjusted pathing to @ format

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,27 @@
-import { defineConfig } from 'vite'
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    react(),
-    babel({ presets: [reactCompilerPreset()] })
+    react({
+      babel: {
+        plugins: [["babel-plugin-react-compiler"]],
+      },
+    }),
   ],
-})
+  resolve: {
+    alias: {
+      "@design": resolve(__dirname, "./src/app/components/design"),
+      "@functional": resolve(__dirname, "./src/app/components/functional"),
+      "@pages": resolve(__dirname, "./src/app/pages"),
+      "@style": resolve(__dirname, "./src/style"),
+      "@core": resolve(__dirname, "./src/core"),
+    },
+  },
+});
