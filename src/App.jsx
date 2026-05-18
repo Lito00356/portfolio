f942eb15fdacaf51793a0cc7d4bf9dd7e272da9f
lito00356 <tomasz.liksza@gmail.com>
2026-05-18 22:38:30 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>Get started</h1>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
+        </p>
+      </div>
+
+      <div className="ticks"></div>
+    </>
+  );
+}
+
+export default App;
