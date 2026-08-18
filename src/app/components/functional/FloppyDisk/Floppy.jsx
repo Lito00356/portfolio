debcb4355ddf10bf60481d3dd948bac63e5f1ed7
lito00356 <tomasz.liksza@gmail.com>
2026-08-18 15:23:21 +0200

    new load in and clone of floppy | added seperate texures to sticker and adjusted colors and flip

--- src/app/components/functional/FloppyDisk/Floppy.jsx
+++ src/app/components/functional/FloppyDisk/Floppy.jsx
@@ -1,9 +1,38 @@
 import { MODELS } from "@lib/paths";
-import { useGLTF } from "@react-three/drei";
+import { useGLTF, useTexture } from "@react-three/drei";
+import { useEffect, useMemo } from "react";
+import { SRGBColorSpace } from "three";
 
-export const Floppy = (props) => {
+const floppyScale = 1;
+
+export const Floppy = ({ texturePath, ...props }) => {
   const { scene } = useGLTF(MODELS.floppy);
-  return <primitive object={scene} scale={1} {...props} />;
+  const stickerTexture = useTexture(texturePath, (texture) => {
+    texture.flipY = false;
+    texture.colorSpace = SRGBColorSpace;
+  });
+
+  const clonedScene = useMemo(() => {
+    const clone = scene.clone(true);
+    clone.traverse((child) => {
+      if (child.isMesh && child.material.name === "Sticker") {
+        child.material = child.material.clone();
+      }
+    });
+
+    return clone;
+  }, [scene]);
+
+  useEffect(() => {
+    clonedScene.traverse((child) => {
+      if (child.isMesh && child.material.name === "Sticker") {
+        child.material.map = stickerTexture;
+        child.material.needsUpdate = true;
+      }
+    });
+  }, [clonedScene, stickerTexture]);
+
+  return <primitive object={clonedScene} scale={floppyScale} {...props} />;
 };
 
 export default Floppy;
--- src/app/pages/Landing/Landing.jsx
+++ src/app/pages/Landing/Landing.jsx
@@ -2,6 +2,7 @@ import { Canvas } from "@react-three/fiber";
 import { Environment, OrbitControls } from "@react-three/drei";
 import { Suspense } from "react";
 import Floppy from "@functional/FloppyDisk/Floppy";
+import { STICKERS } from "@lib/paths";
 
 const Landing = () => {
   return (
@@ -10,7 +11,9 @@ const Landing = () => {
       <Environment preset="studio" />
 
       <Suspense fallback={null}>
-        <Floppy position={[0, 0, 0]} />
+        <Floppy position={[0, 0, 0]} texturePath={STICKERS.vfx} />
+        <Floppy position={[1, 0, 0]} texturePath={STICKERS.coding} />
+        <Floppy position={[-1, 0, 0]} texturePath={STICKERS.about} />
       </Suspense>
       <OrbitControls />
     </Canvas>
